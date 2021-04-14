48b97d6f808d3755734fe3ae7d34304b24abc850
Vihoc <cwh5635@live.cn>
2021-04-14 12:16:20 +0800

    add stack and queue

---
+++ queue.h
@@ -0,0 +1,41 @@
+/**
+*使用循環鏈表實現的隊列,接口同stl
+*author: vihoc
+*/
+
+#ifndef _VIHO_STRUCTURES_QUEUE_H
+#define _VIHO_STRUCTURES_QUEUE_H
+
+#include <cstdint>
+
+#include <doubly_circular_list.h>
+#include <traits.h>
+
+namespace VihoStructures {
+
+template <typename T, typename Container>
+class QueueWrapper {
+public:
+	void push(const T& data) { return cont.push_back(data); }
+	T pop() { return cont.pop_front(); }
+	T& front() { return cont.front(); }
+	const T& front() const { return cont.front(); }
+	T& back() { return cont.back(); }
+	const T& back() const { return cont.back(); }
+	void clear() { return cont.clear(); }
+	std::size_t size() const { return cont.size(); }
+
+private:
+	Container cont;
+};
+
+template <typename T>
+class Queue : public QueueWrapper<T, DoublyCircularList<T>> {};
+
+}  // namespace structures
+
+/* name trait */
+template <>
+const std::string traits::type<VihoStructures::Queue>::name = "Queue";
+
+#endif
---
+++ stack.h
@@ -0,0 +1,41 @@
+/**
+*
+*基於數組實現棧, 接口同stl
+*
+*author: vihoc
+*/
+
+#ifndef STRUCTURES_STACK_H
+#define STRUCTURES_STACK_H
+
+#include <cstdint>
+
+#include <array_list.h>
+#include <traits.h>
+
+namespace VihoStructures {
+
+template <typename T, typename Container>
+class StackWrapper {
+public:
+	void push(const T& data) { return cont.push_back(data); }
+	T pop() { return cont.pop_back(); }
+	T& top() { return cont.back(); }
+	const T& top() const { return cont.back(); }
+	void clear() { return cont.clear(); }
+	std::size_t size() const { return cont.size(); }
+
+private:
+	Container cont;
+};
+
+template <typename T>
+class Stack : public StackWrapper<T, ArrayList<T>> {};
+
+}  // namespace structures
+
+/* name trait */
+template <>
+const std::string traits::type<VihoStructures::Stack>::name = "Stack";
+
+#endif
